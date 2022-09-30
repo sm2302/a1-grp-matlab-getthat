c35b4cf626ad091fbad58f0d9b5bfbbee1287f0a
AfiqahSofry <19b2102@ubd.edu.bn>
2022-09-30 21:16:21 +0800

    updated codes and added comments

--- solutions.m
+++ solutions.m
@@ -13,25 +13,28 @@ current_gen(2:N+1, 2:N+1) = sprand(N, N, 0.5);
 
 % Counting live neighbours
 for i = 1:N;
-    for r = 2:N+1;
-        for c = 2:N+1;
+    for r = 2:N+1; % rows of the current_gen matrix
+        for c = 2:N+1; % columns of the current_gen matrix
+            % counting diagonal living neighbours
             diag = current_gen(r-1, c-1) + current_gen(r-1, c+1) + current_gen(r+1, c-1) + current_gen(r+1, c+1);
+            % counting horizontal & vertical living neighbours
             horver = current_gen(r-1, c) + current_gen(r+1, c) + current_gen(r, c-1) + current_gen(r, c+1);
+            % no.of living neighbours
             num_alive = diag + horver;
         endfor
     endfor
 endfor
 
-% updating the new cells according to the rules
+% Updating the new cells according to the rules
 for k = 1:5;
     new_gen = current_gen;
     for r = 2:N+1;
       for c = 2:N+1;
-    if (new_gen(r,c) == 1) && ((num_alive == 2) || (num_alive == 3))
+    if (current_gen(r,c) == 1) && ((num_alive == 2) || (num_alive == 3))
        (new_gen(r,c) == 1);
-    elseif (new_gen(r,c) == 1) && ((num_alive < 2) || (num_alive > 3))
+    elseif (current_gen(r,c) == 1) && ((num_alive < 2) || (num_alive > 3))
            (new_gen(r,c) == 0);
-    elseif (new_gen(r,c) == 0) && (num_alive == 3)
+    elseif (current_gen(r,c) == 0) && (num_alive == 3)
            (new_gen(r,c) == 1);
     endif
       end
