4fb4888f8686777fa6a45abcf559fcc45f427f75
AfiqahSofry <19b2102@ubd.edu.bn>
2022-09-30 19:51:55 +0800

    added generating random matrix

--- solutions.m
+++ solutions.m
@@ -1,5 +1,13 @@
+clear;
+clc;
+close;
+
 % ASSIGNMENT 1 MATLAB
 % CONWAY'S GAME OF LIFE
 
-% Initialisation
+% Initialisation: generate random matrix of size N
+N = 10;
+current_gen = zeros(N+2, N+2);
+current_gen(2:N+1, 2:N+1) = sprand(N, N, 0.5);
+
 
