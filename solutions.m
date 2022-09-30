785d14943a062ce7d3f7fb0b609fae0cb1e23366
alyaadis <19b4024@ubd.edu.bn>
2022-09-30 21:04:21 +0800

    Added rules of game

--- solutions.m
+++ solutions.m
@@ -3,6 +3,7 @@ clc;
 close;
 
 % ASSIGNMENT 1 MATLAB
+% GROUP: GetThat°
 % CONWAY'S GAME OF LIFE
 
 % Initialisation: generate random matrix of size N
@@ -20,3 +21,19 @@ for i = 1:N;
         endfor
     endfor
 endfor
+
+% updating the new cells according to the rules
+for k = 1:5;
+    new_gen = current_gen;
+    for r = 2:N+1;
+      for c = 2:N+1;
+    if (new_gen(r,c) == 1) && ((num_alive == 2) || (num_alive == 3))
+       (new_gen(r,c) == 1);
+    elseif (new_gen(r,c) == 1) && ((num_alive < 2) || (num_alive > 3))
+           (new_gen(r,c) == 0);
+    elseif (new_gen(r,c) == 0) && (num_alive == 3)
+           (new_gen(r,c) == 1);
+    endif
+      end
+   end
+end
