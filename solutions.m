b7e658907583c253ea43e083b681a3c6bf9eb486
AthiraHarman <20b2047@ubd.edu.bn>
2022-09-30 20:33:12 +0800

    Counting Live Neighbours

--- solutions.m
+++ solutions.m
@@ -10,4 +10,13 @@ N = 10;
 current_gen = zeros(N+2, N+2);
 current_gen(2:N+1, 2:N+1) = sprand(N, N, 0.5);
 
-
+% Counting live neighbours
+for i = 1:N;
+    for r = 2:N+1;
+        for c = 2:N+1;
+            diag = current_gen(r-1, c-1) + current_gen(r-1, c+1) + current_gen(r+1, c-1) + current_gen(r+1, c+1);
+            horver = current_gen(r-1, c) + current_gen(r+1, c) + current_gen(r, c-1) + current_gen(r, c+1);
+            num_alive = diag + horver;
+        endfor
+    endfor
+endfor
